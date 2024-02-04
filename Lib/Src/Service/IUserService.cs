59e6aa224cd2c101e8b1682c0aa8e49264f0bee8
Sehmus Acar <shmsacr@gmail.com>
2024-02-04 23:11:26 +0300

    create user interface

---
+++ Lib/Src/Service/IUserService.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using todocrud.Lib.Src.DTOs.UserDTO;
+using todocrud.Lib.Src.Model;
+
+namespace todocrud.Lib.Src.Service;
+
+public interface IUserService
+{
+    public Task<UserModel?> GetUserById(ObjectId id);
+    public Task<IResult> Register(RegisterDTO userModel);
+    public Task<bool> UpdateUser(ObjectId id, UserModel userModel);
+    public Task<bool> DeleteUser(ObjectId id);
+    public Task<UserModel> GetUserByEmail(string email);
+    public Task<List<UserModel>> GetAllUsers();
+    public Task<IResult> LoginUser(LoginDTO loginDTO);
+    
+    
+}
