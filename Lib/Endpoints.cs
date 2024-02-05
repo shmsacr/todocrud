1eb5e2987bade0d53e2f3e474ee95c4f7da2d513
Sehmus Acar <shmsacr@gmail.com>
2024-02-05 23:08:36 +0300

    add endpoint all method for todo

--- Lib/Endpoints.cs
+++ Lib/Endpoints.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using MongoDB.Bson;
+using todocrud.Lib.Src.DTOs.TodoDTO;
 using todocrud.Lib.Src.DTOs.UserDTO;
 using todocrud.Lib.Src.EnpointHandlers;
 using todocrud.Lib.Src.Model;
@@ -10,6 +13,7 @@ public class Endpoints
 {
     public static void EnpointsRouter(WebApplication app)
     {
+        #region UserEndpoints
         var user = app.MapGroup("api/users");
         user.MapGet("getAllUsers", async (UserService userService) =>
         {
@@ -23,5 +27,31 @@ public class Endpoints
         {
             return await new UserEndpointHandlers(userService).LoginUser(loginDTO);
         }).AllowAnonymous();
+        #endregion 
+        /////////////////////////////////////////////////////////////////////////////
+        #region TodoEndpoints
+        var todo = app.MapGroup("api/todos");
+        todo.MapGet("getAllTodos", async (TodoService todoService) =>
+        {
+            return await new TodoEndpointHandlers(todoService).GetAllTodos();
+        }).RequireAuthorization();
+        todo.MapGet("getTodoById/{id}", async (TodoService todoService, string id) =>
+        {
+            return await new TodoEndpointHandlers(todoService).GetTodoById(id);
+        }).RequireAuthorization();
+        todo.MapPost("createTodo", async (TodoService todoService,[FromBody] CreateTodoRequest todoModel) =>
+        {
+            return await new TodoEndpointHandlers(todoService).CreateTodoModel(todoModel);
+        }).RequireAuthorization();
+        todo.MapPut("updateTodo/{id}", async (TodoService todoService, string id, UpdateDTO todoModel) =>
+        {
+            return await new TodoEndpointHandlers(todoService).UpdateTodoModel(id, todoModel);
+        }).RequireAuthorization();
+        todo.MapDelete("deleteTodo/{id}", async (TodoService todoService, string id) =>
+        {
+            return await new TodoEndpointHandlers(todoService).DeleteTodoModel(id);
+        }).RequireAuthorization();
+        #endregion
+
     }
 }
