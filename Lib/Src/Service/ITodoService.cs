4fd737fdc2632a8ff73deb967654b0f588119e9a
Sehmus Acar <shmsacr@gmail.com>
2024-02-11 16:52:42 +0300

    refactor todo service return type

--- Lib/Src/Service/ITodoService.cs
+++ Lib/Src/Service/ITodoService.cs
@@ -1,14 +1,17 @@
 using MongoDB.Bson;
+using todocrud.Lib.Src.DTOs.TodoDTO;
 using todocrud.Lib.Src.Model;
 
 namespace todocrud.Lib.Src.Service;
 
 public interface ITodoService
 {
-    Task<List<TodoModel>> GetAllTodos();
+    Task<List<ResponseAllTodoDto>> GetAllTodos();
     Task<TodoModel> GetTodoById(ObjectId id);
     Task<TodoModel?> CreateTodoModel(TodoModel todoModel);
-    Task <bool> UpdateTodoModel(ObjectId id, TodoModel todoModel);
-    Task <bool>DeleteTodoModel(ObjectId id);
+    Task <IResult> UpdateTodoModel(ObjectId id, TodoModel todoModel);
+    Task <IResult>DeleteTodoModel(ObjectId id);
+    Task<List<ResponseAllTodoDto>?> GetTodoByUserId();
+    Task<IResult> UpdateTodoStatus(ObjectId id, bool status);
     
 }
