ff51b95fdcca113edf729edd858d82505a8af789
Sehmus Acar <shmsacr@gmail.com>
2024-02-11 16:55:17 +0300

    add id update todo dto

--- Lib/Src/DTOs/TodoDTO/UpdateDTO.cs
+++ Lib/Src/DTOs/TodoDTO/UpdateDTO.cs
@@ -1,8 +1,8 @@
 namespace todocrud.Lib.Src.DTOs.TodoDTO;
 public class UpdateDTO
 {
+    public string? Id { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
-    public bool IsCompleted { get; set; } 
-    
+    public bool IsCompleted { get; set; }
 }
